68e24f608608d430a832ceb540dea8abbbfc0e5c
Benjamin Pavie <benjamin.pavie@gmail.com>
2020-01-20 14:39:58 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# W_Neuron3dTree_vaa3d_most
+Vaa3d MOST Vessel Tracer (MOST) dockerised workflow for BIAFLOWS
+
+### Full Description :
+ See on the [Bioimage Informatics Search Engine (BISE)](https://biii.eu) in the [most-raytracer](http://biii.eu/most-raytracer) section or in the [github repository](https://github.com/Vaa3D/vaa3d_tools/tree/master/released_plugins/v3d_plugins/neurontracing_MOST)
+
+### Publication:
+Link : http://www.sciencedirect.com/science/article/pii/S1053811913010598
---
+++ workflow.py
@@ -0,0 +1,59 @@
+import os
+import time
+import sys
+from subprocess import call
+from cytomine.models import Job
+
+import imageio as imgio
+import numpy as np
+import skimage
+from swc_to_tiff_stack import swc_to_tiff_stack
+import tifffile as tiff
+
+
+def workflow(in_images, out_path,
+                 threshold, seed, slip):
+    print("Starting workflow!")
+    
+    for neubias_input_image in in_images:
+        print('---------------------------')
+        file_path=neubias_input_image.filepath
+        filename = neubias_input_image.filename
+        out_file_path = os.path.join(out_path, filename)
+        print('doing '+file_path)
+        #Invert the xy axis by 180 degrees / in other words, flip the image vertically
+        print('invert the xy axis by 180 degrees for '+file_path)
+        
+        #reads image and rotates it with numpy.flip
+        img = skimage.external.tifffile.imread(file_path)
+        img = np.flip(img,axis=1)
+        skimage.external.tifffile.imsave(out_file_path,img)
+        print("Finished running: 180 degrees image rotation in xy axis")
+        
+        #Compute the neuron tracing
+        #command = "/usr/bin/xvfb-run Vaa3D_CentOS_64bit_v3.458/vaa3d -x libvn2 -f app1 -i " + \
+        #    out_file_path + " -o " + out_file_path[:-4]+ ".swc"
+
+        #/home/xxx/bin/Vaa3D_CentOS_64bit_v3.458/start_vaa3d.sh -x "home/xxx/bin/Vaa3D_CentOS_64bit_v3.458/plugins/neuron_tracing/MOST_tracing/libmostVesselTracer.so" -f MOST_trace -i "/home/xxx/Documents/TREES_Image3.ome.tif" -p 1,20,20,20
+        command = "/usr/bin/xvfb-run Vaa3D_CentOS_64bit_v3.458/vaa3d -x /Vaa3D_CentOS_64bit_v3.458/plugins/neuron_tracing/MOST_tracing/libmostVesselTracer.so -f MOST_trace -i {} -o {}.swc " \
+                  "-p \"\" 1 {} {} {}".format(out_file_path, out_file_path[:-4],
+                    threshold, seed, slip)
+
+
+        return_code = call(command, shell=True, cwd="/") # waits for the subprocess to return
+        #Move the result file to the output name
+        os.rename("{}_MOST.swc".format(out_file_path),"{}.swc".format(out_file_path[:-4]))
+        
+        #time.sleep(10)#Wait 10 seconds because it can't process all the images for some reason
+        print("Finished running filament tracing :"+command)
+        im_size = imgio.volread(out_file_path).shape #Size is Depth,Height,Width
+        im_size = im_size[::-1] #Invert the size order to Width,Height,Depth
+        #Rename the swc file form *.tif_ini.swc to *.swc
+        #Needed for some vaa3d workflow where the output path is not taken into account.
+        print("Run:"+' swc_to_tiff_stack('+ out_file_path[:-4] +'.swc, '+ out_path +','+ str(im_size)+')')
+        # Convert the .swc tracing result to tiff stack files
+        swc_to_tiff_stack(out_file_path[:-4]+ ".swc", out_path, im_size)
+        print("Finished running conversion of swc to tiff stack")
+        #TODO: error handling...
+        
+    print("Done")    
